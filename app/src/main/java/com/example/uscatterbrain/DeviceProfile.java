b6ddf29b1b105927ec689ed94e2556291787edf9
Alex Ballmer <gnu3ra@riseup.net>
2019-08-10 23:05:01 +0000

    modify deviceprofile to support current simplified protocol

--- app/src/main/java/com/example/uscatterbrain/DeviceProfile.java
+++ app/src/main/java/com/example/uscatterbrain/DeviceProfile.java
@@ -1,6 +1,8 @@
 package com.example.uscatterbrain;
 
 
+import java.util.UUID;
+
 /**
  * General device information and settings storage.
  * Used to refer to a device
@@ -8,68 +10,34 @@ package com.example.uscatterbrain;
 
 public class DeviceProfile {
 
-    private byte[] luid;
-
-
-    public enum deviceType {
-        ANDROID, IOS, LINUX
-    }
-
-
-    public enum MobileStatus {
-        STATIONARY, MOBILE, VERYMOBILE
-    }
-
+    private UUID luid;
 
     public enum HardwareServices {
-        WIFIP2P, WIFICLIENT, WIFIAP, BLUETOOTH,
-        BLUETOOTHLE, INTERNET
+        WIFIP2P, BLUETOOTHCLASSIC,
+        BLUETOOTHLE, ULTRASOUND
     }
-
-
-    private deviceType type;
-    private MobileStatus status;
     private HardwareServices services;
 
     private final byte protocolVersion = 2;
-    private byte congestion;
-    public DeviceProfile (deviceType type, MobileStatus status, HardwareServices services, byte[] id) {
-        this.type = type;
+
+    public DeviceProfile (HardwareServices services, UUID id) {
         this.services = services;
-        this.status = status;
-        congestion = 0;
         this.luid = id;
     }
 
-
-    public void  update(deviceType type, MobileStatus status, HardwareServices services) {
-        this.type = type;
+    public void  update(HardwareServices services) {
         this.services = services;
-        this.status = status;
-    }
-
-    public deviceType getType() {
-        return type;
     }
 
     public HardwareServices getServices() {
         return services;
     }
 
-    public MobileStatus getStatus() {
-        return status;
-    }
-
     public byte getProtocolVersion() {
         return protocolVersion;
     }
 
-    public byte getCongestion() { return congestion; }
+    public UUID getLUID(){ return this.luid;}
 
-
-    public void setCongestion(byte congestion) { this.congestion = congestion; }
-    public byte[] getLUID(){ return this.luid;}
-
-
-    public void setLUID(byte[] id){this.luid = id;}
+    public void setLUID(UUID id){this.luid = id;}
 }
