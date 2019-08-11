9a27bb31387b80f554156638a29cbc0b5fa5ff3e
Alex Ballmer <gnu3ra@riseup.net>
2019-08-11 17:51:31 -0500

    Bluetooth LE advertise stop/start

--- app/src/main/java/com/example/uscatterbrain/network/bluetoothLE/ScatterBluetoothLEManager.java
+++ app/src/main/java/com/example/uscatterbrain/network/bluetoothLE/ScatterBluetoothLEManager.java
@@ -1,4 +1,64 @@
 package com.example.uscatterbrain.network.bluetoothLE;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.AdvertisingSet;
+import android.bluetooth.le.AdvertisingSetCallback;
+import android.bluetooth.le.AdvertisingSetParameters;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.bluetooth.le.BluetoothLeScanner;
+
 public class ScatterBluetoothLEManager {
+    public static final String TAG = "BluetoothLE";
+
+    BluetoothLeAdvertiser mAdvertiser;
+    BluetoothLeScanner mScanner;
+    BluetoothAdapter mAdapter;
+    AdvertisingSet current;
+
+    public ScatterBluetoothLEManager() {
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mAdvertiser = mAdapter.getBluetoothLeAdvertiser();
+        mScanner = mAdapter.getBluetoothLeScanner();
+    }
+
+    public void startLEAdvertise() {
+        AdvertisingSetParameters parameters = (new AdvertisingSetParameters.Builder())
+                .setLegacyMode(true) // True by default, but set here as a reminder.
+                .setConnectable(true)
+                .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
+                .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
+                .build();
+
+        AdvertiseData data = (new AdvertiseData.Builder()).setIncludeDeviceName(true).build();
+
+        AdvertisingSetCallback callback = new AdvertisingSetCallback() {
+            @Override
+            public void onAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, int status) {
+                super.onAdvertisingSetStarted(advertisingSet, txPower, status);
+                current = advertisingSet;
+            }
+        };
+
+        mAdvertiser.startAdvertisingSet(parameters, data, null,
+                null, null, callback);
+    }
+
+    public void stopLEAdvertise() {
+        AdvertiseCallback callback = new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                super.onStartSuccess(settingsInEffect);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                super.onStartFailure(errorCode);
+            }
+        };
+
+        mAdvertiser.stopAdvertising(callback);
+    }
 }
