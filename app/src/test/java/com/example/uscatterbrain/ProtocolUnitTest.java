2a298186a24073bf7c2efa35db33c20de34cb13c
Alex Ballmer <gnu3ra@riseup.net>
2019-08-11 01:10:50 +0000

    blockdatapacket protobuf passes first unit test

--- app/src/test/java/com/example/uscatterbrain/ProtocolUnitTest.java
+++ app/src/test/java/com/example/uscatterbrain/ProtocolUnitTest.java
@@ -1,13 +1,23 @@
 package com.example.uscatterbrain;
 
+import com.example.uscatterbrain.network.BlockDataPacket;
+import com.google.protobuf.InvalidProtocolBufferException;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.TempDirectory;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 @RunWith(RobolectricTestRunner.class)
@@ -17,6 +27,7 @@ public class ProtocolUnitTest {
     private Random localRandom;
     private TempDirectory td;
     private final String TMPDIR = "tmpdir3";
+    private final String APPNAME = "ScatterBean";
 
     @Before
     public void setup() {
@@ -24,10 +35,27 @@ public class ProtocolUnitTest {
         td = new TempDirectory(TMPDIR);
     }
 
-
     @After
     public void cleanup() {
         td.destroy();
+    }
+
+
+    @Test
+    public void blockDataSerializesByteArray() {
+        byte[] bytes = new byte[1024];
+        localRandom.nextBytes(bytes);
+        UUID uid1 = UUID.randomUUID();
+        UUID uid2 = UUID.randomUUID();
+
+        try {
+            BlockDataPacket bd = new BlockDataPacket(APPNAME, uid1, uid2, false);
+            bd.setData(bytes);
+            byte[] res = bd.getBytes();
+            BlockDataPacket bd2 = new BlockDataPacket(res);
+        } catch (InvalidProtocolBufferException p) {
+            Assert.fail();
+        }
     }
 
 }
