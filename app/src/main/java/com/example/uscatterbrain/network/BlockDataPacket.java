97c5e2afa24a5bcc49301ae9632e904539ea8279
Alex Ballmer <gnu3ra@riseup.net>
2019-08-11 00:17:10 +0000

    blockdata packet wrapper class, some changes to protocol specs (separate protobuf message for multipart blockdata

--- app/src/main/java/com/example/uscatterbrain/network/BlockDataPacket.java
+++ app/src/main/java/com/example/uscatterbrain/network/BlockDataPacket.java
@@ -1,5 +1,50 @@
 package com.example.uscatterbrain.network;
 
+import com.example.uscatterbrain.ScatterProto;
+import com.google.protobuf.ByteString;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class BlockDataPacket {
-    //TODO: initialize protobuf
+    private ScatterProto.BlockData.Builder proto;
+    private ScatterProto.BlockData blockdata;
+    private boolean dataready;
+    public BlockDataPacket(String application, UUID to, UUID from, boolean disk) {
+        dataready = false;
+        proto = ScatterProto.BlockData.newBuilder();
+        proto.setApplication(application);
+        proto.setFromLow(from.getLeastSignificantBits());
+        proto.setFromHigh(from.getMostSignificantBits());
+        proto.setToLow(from.getLeastSignificantBits());
+        proto.setToHigh(to.getMostSignificantBits());
+        proto.setTodisk(disk);
+    }
+
+    public void setData(InputStream istream) throws IOException {
+        proto.setData(ByteString.readFrom(istream));
+        blockdata = proto.build();
+        dataready = true;
+    }
+
+    public void setData(byte[] bdata) {
+        proto.setData(ByteString.copyFrom(bdata));
+        blockdata = proto.build();
+        dataready = true;
+    }
+
+
+    public void writeToOutputStream(OutputStream out) throws IOException {
+        if(dataready)
+            blockdata.writeTo(out);
+    }
+
+    public byte[] getBytes() {
+        if(dataready)
+            return blockdata.toByteArray();
+        else
+            return null;
+    }
 }
