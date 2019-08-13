3c98da32ad1076267ce1ea8cbd4dc1dd4ebabb85
Alex Ballmer <gnu3ra@riseup.net>
2019-08-13 01:29:29 +0000

    allow older SDK versions, but don't start LE advertise on an unsupported device

--- app/src/main/java/com/example/uscatterbrain/network/bluetoothLE/ScatterBluetoothLEManager.java
+++ app/src/main/java/com/example/uscatterbrain/network/bluetoothLE/ScatterBluetoothLEManager.java
@@ -1,5 +1,7 @@
 package com.example.uscatterbrain.network.bluetoothLE;
 
+import android.app.Activity;
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
@@ -9,7 +11,12 @@ import android.bluetooth.le.AdvertisingSetCallback;
 import android.bluetooth.le.AdvertisingSetParameters;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.ParcelUuid;
+
+import com.example.uscatterbrain.ScatterRoutingService;
 
 import java.util.UUID;
 
@@ -18,37 +25,50 @@ public class ScatterBluetoothLEManager {
     public static final int SERVICE_ID = 0xFEEF;
     public static final UUID SERVICE_UUID = UUID.fromString("9a21e79f-4a6d-4e28-95c6-257f5e47fd90");
 
+    private final int REQUEST_ENABLE_BT = 1;
+
     BluetoothLeAdvertiser mAdvertiser;
     BluetoothLeScanner mScanner;
     BluetoothAdapter mAdapter;
     AdvertisingSet current;
 
-    public ScatterBluetoothLEManager() {
+    public ScatterBluetoothLEManager(Service mService) {
         mAdapter = BluetoothAdapter.getDefaultAdapter();
         mAdvertiser = mAdapter.getBluetoothLeAdvertiser();
         mScanner = mAdapter.getBluetoothLeScanner();
     }
 
+    public void enableBluetooth(Activity activity) {
+        if (mAdapter == null || !mAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+    }
+
     public void startLEAdvertise() {
-        AdvertisingSetParameters parameters = (new AdvertisingSetParameters.Builder())
-                .setLegacyMode(true) // True by default, but set here as a reminder.
-                .setConnectable(true)
-                .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
-                .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
-                .build();
+        if(Build.VERSION.SDK_INT >= 26) {
+            AdvertisingSetParameters parameters = (new AdvertisingSetParameters.Builder())
+                    .setLegacyMode(true) // True by default, but set here as a reminder.
+                    .setConnectable(true)
+                    .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
+                    .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
+                    .build();
 
-        AdvertiseData data = (new AdvertiseData.Builder()).setIncludeDeviceName(true).build();
+            AdvertiseData data = (new AdvertiseData.Builder()).setIncludeDeviceName(true).build();
 
-        AdvertisingSetCallback callback = new AdvertisingSetCallback() {
-            @Override
-            public void onAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, int status) {
-                super.onAdvertisingSetStarted(advertisingSet, txPower, status);
-                current = advertisingSet;
-            }
-        };
+            AdvertisingSetCallback callback = new AdvertisingSetCallback() {
+                @Override
+                public void onAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, int status) {
+                    super.onAdvertisingSetStarted(advertisingSet, txPower, status);
+                    current = advertisingSet;
+                }
+            };
 
-        mAdvertiser.startAdvertisingSet(parameters, data, null,
-                null, null, callback);
+            mAdvertiser.startAdvertisingSet(parameters, data, null,
+                    null, null, callback);
+        }
     }
 
     public void stopLEAdvertise() {
