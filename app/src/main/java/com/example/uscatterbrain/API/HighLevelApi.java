032fd54fc14412773f60606914824c03965a8b68
Alex Ballmer <gnu3ra@riseup.net>
2019-09-09 09:54:49 -0500

    HighlevelAPI from old scatterbrain

---
+++ app/src/main/java/com/example/uscatterbrain/API/HighLevelApi.java
@@ -0,0 +1,50 @@
+package com.example.uscatterbrain.API;
+
+import android.content.SharedPreferences;
+
+
+import com.example.uscatterbrain.DeviceProfile;
+import com.example.uscatterbrain.network.BlockDataPacket;
+
+import java.io.InputStream;
+
+/**
+ * basic interface to the Scatterbrain protocol, to be used by
+ * external applications
+ */
+
+public interface HighLevelApi {
+
+    //system
+    void startService();
+    void stopService();
+    SharedPreferences getPref();
+    void setPref(SharedPreferences pref);
+    DeviceProfile getProfile();
+    void setProfile(DeviceProfile prof);
+    ScatterTransport[] getTransports();
+    String getScatterApplication();
+    void setScatterApplication(String application);
+
+    //peers
+    void scanOn(ScatterTransport transport);
+    void scanOff(ScatterTransport transport);
+    DeviceProfile[] getPeers();
+
+    //communications
+    boolean sendDataDirected(DeviceProfile target, byte[] data);
+    void sendDataMulticast(byte[] data);
+    boolean sendFileDirected(DeviceProfile target, InputStream file, String name, long len);
+    void sendFileMulticast(InputStream file, String name, long len); //TODO: java file object
+    void registerOnRecieveCallback(OnRecieveCallback callback);
+
+
+    //datastore
+    BlockDataPacket[] getTopMessages(int num);
+    BlockDataPacket[] getRandomMessages(int num);
+
+    //datastore systems tasks
+    void flushDatastore();
+    void setDatastoreLimit(int limit);
+    int getDatastoreLimit();
+}
