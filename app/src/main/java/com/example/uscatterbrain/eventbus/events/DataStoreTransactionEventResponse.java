90a626ad7ab0bc205f89ccf7eb044c8b2cdc994a
Alex Ballmer <gnu3ra@riseup.net>
2020-01-28 04:11:37 +0000

    Add event response classes (all generic for now)

---
+++ app/src/main/java/com/example/uscatterbrain/eventbus/events/DataStoreTransactionEventResponse.java
@@ -0,0 +1,17 @@
+package com.example.uscatterbrain.eventbus.events;
+
+import com.example.uscatterbrain.eventbus.ScatterEventBusEvent;
+import com.example.uscatterbrain.eventbus.ScatterEventBusResponse;
+
+public class DataStoreTransactionEventResponse implements ScatterEventBusResponse {
+    private StatusCode mStatusCode;
+    private ScatterEventBusEvent mEvent;
+
+    public StatusCode getStatusCode() {
+        return mStatusCode;
+    }
+
+    public ScatterEventBusEvent getInReplyTo() {
+        return mEvent;
+    }
+}
