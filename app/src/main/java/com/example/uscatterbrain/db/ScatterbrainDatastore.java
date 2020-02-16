06e4c82d46f71215dee2a4a7bd365774ab3ca88c
Alex Ballmer <gnu3ra@riseup.net>
2020-02-16 00:06:29 -0500

    Execute all insert operation on separate thread

--- app/src/main/java/com/example/uscatterbrain/db/ScatterbrainDatastore.java
+++ app/src/main/java/com/example/uscatterbrain/db/ScatterbrainDatastore.java
@@ -1,7 +1,9 @@
 package com.example.uscatterbrain.db;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.room.Entity;
 import androidx.room.Room;
 
 import com.example.uscatterbrain.db.entities.DiskFiles;
@@ -12,14 +14,18 @@ import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ScatterbrainDatastore {
 
     private Datastore mDatastore;
+    private Executor executor;
     public static final String DATABASE_NAME = "scatterdb";
 
     public ScatterbrainDatastore(Context ctx) {
         mDatastore = Room.databaseBuilder(ctx, Datastore.class, DATABASE_NAME).build();
+        executor = Executors.newSingleThreadExecutor();
     }
 
     public void insertMessage(List<ScatterMessage> messages) throws DatastoreInsertException {
@@ -37,43 +43,48 @@ public class ScatterbrainDatastore {
             throw new DatastoreInsertException();
         }
 
-        mDatastore.scatterMessageDao().insertMessages(message);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mDatastore.scatterMessageDao().insertMessages(message);
+            }
+        });
     }
 
     public  void insertIdentity(Identity[] identities) {
-        mDatastore.identityDao().insertAll(identities);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mDatastore.identityDao().insertAll(identities);
+            }
+        });
     }
 
     public void insertIdentity(Identity identity) {
-        mDatastore.identityDao().insertAll(identity);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mDatastore.identityDao().insertAll(identity);
+            }
+        });
     }
 
     public void insertFile(DiskFiles files) {
-        mDatastore.diskFilesDao().insertAll(files);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mDatastore.diskFilesDao().insertAll(files);
+            }
+        });
     }
 
     public void insertFile(List<DiskFiles> files) {
-        mDatastore.diskFilesDao().insertAll(files);
-    }
-
-    public void insertObject(DatastoreEntity et) throws DatastoreInsertException {
-        DatastoreEntity.entityType type = et.getType();
-
-        switch(type) {
-            case TYPE_MESSAGE:
-                insertMessage((ScatterMessage)et);
-                break;
-            case TYPE_IDENTITY:
-                insertIdentity((Identity)et);
-                break;
-
-            case TYPE_FILE:
-                insertFile((DiskFiles) et);
-                break;
-
-            default:
-                throw new DatastoreInsertException();
-        }
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mDatastore.diskFilesDao().insertAll(files);
+            }
+        });
     }
 
     public static class DatastoreInsertException extends Exception {
