ae24d8dfc6fd37f54c315b776909a2df03346339
Alex Ballmer <gnu3ra@riseup.net>
2020-02-16 22:25:13 -0500

    port datastore unit tests from robolectric to android instrumented test

--- app/src/androidTest/java/com/example/uscatterbrain/DatastoreInstrumentedTest.java
+++ app/src/androidTest/java/com/example/uscatterbrain/DatastoreInstrumentedTest.java
@@ -1,21 +1,36 @@
 package com.example.uscatterbrain;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 
+import androidx.lifecycle.Observer;
+import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 import androidx.test.rule.ServiceTestRule;
 import androidx.test.runner.AndroidJUnitRunner;
 
+import com.example.uscatterbrain.db.Datastore;
+import com.example.uscatterbrain.db.ScatterbrainDatastore;
+import com.example.uscatterbrain.db.entities.DiskFiles;
+import com.example.uscatterbrain.db.entities.Identity;
+import com.example.uscatterbrain.db.entities.ScatterMessage;
+
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -29,10 +44,119 @@ public class DatastoreInstrumentedTest {
     @Rule
     public final ServiceTestRule serviceRule = new ServiceTestRule();
 
-    @Test
-    public void testBindService() throws TimeoutException {
+
+    public void blockForThread() {
+        while(testRunning) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public ScatterRoutingService getService() throws TimeoutException {
         Intent bindIntent = new Intent(ApplicationProvider.getApplicationContext(), ScatterRoutingService.class);
         IBinder binder = serviceRule.bindService(bindIntent);
         ScatterRoutingService service = ((ScatterRoutingService.ScatterBinder)binder).getService();
+        return service;
+    }
+
+    public ScatterMessage defaultMessage() {
+        ScatterMessage sm = new ScatterMessage(new Identity(), new byte[5]);
+        sm.addFile(new DiskFiles());
+        return sm;
+    }
+
+    public List<ScatterMessage> defaultMessages(int count) {
+        List<ScatterMessage> sl = new ArrayList<>();
+        for(int i=0;i<count;i++) {
+            sl.add(defaultMessage());
+        }
+        return sl;
+    }
+
+    static volatile boolean testRunning = false;
+
+
+    @Test
+    public void publicApiInsertsMessage() {
+        ScatterbrainDatastore datastore = new ScatterbrainDatastore(ApplicationProvider.getApplicationContext());
+        List<ScatterMessage> sms = defaultMessages(1);
+
+        try {
+            testRunning = true;
+            datastore.insertMessage(sms, new ScatterbrainDatastore.DatastoreInsertUpdateCallback<List<Long>>() {
+                @Override
+                public void onRowUpdate(List<Long> rowids) {
+                    assertThat(rowids.size(), is(1));
+                    testRunning = false;
+                }
+            });
+
+            blockForThread();
+
+        }
+        catch(ScatterbrainDatastore.DatastoreInsertException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void publicApiQueryMessageByIdentity() {
+        ScatterbrainDatastore datastore = new ScatterbrainDatastore(ApplicationProvider.getApplicationContext());
+        ScatterMessage sm = defaultMessage();
+        try {
+            testRunning = true;
+            datastore.insertMessage(sm, new ScatterbrainDatastore.DatastoreInsertUpdateCallback<Long>() {
+                @Override
+                public void onRowUpdate(Long rowids) {
+                    assertThat(rowids, not(0L));
+                    testRunning = false;
+
+                }
+            });
+
+            blockForThread();
+        } catch (ScatterbrainDatastore.DatastoreInsertException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void topRandomMessagesWork() throws TimeoutException {
+        ScatterRoutingService service = getService();
+        ScatterbrainDatastore datastore = new ScatterbrainDatastore(ApplicationProvider.getApplicationContext());
+        List<ScatterMessage> messages = defaultMessages(20);
+        try {
+            testRunning = true;
+            datastore.insertMessage(messages, new ScatterbrainDatastore.DatastoreInsertUpdateCallback<List<Long>>() {
+                @Override
+                public void onRowUpdate(List<Long> rowids) {
+                    assertThat(rowids.size(), is(20));
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            datastore.getTopRandomMessages(5).observe(service, new Observer<List<ScatterMessage>>() {
+                                @Override
+                                public void onChanged(List<ScatterMessage> messages) {
+                                    assertThat(messages.size(), is(5));
+                                    testRunning = false;
+                                    System.out.println("done");
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+            blockForThread();
+        } catch(ScatterbrainDatastore.DatastoreInsertException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testBindService() throws TimeoutException {
+
     }
 }
