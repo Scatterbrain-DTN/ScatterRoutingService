d01582879f18c179eee54ba63d0387b158a313d4
Alex Ballmer <gnu3ra@riseup.net>
2020-03-17 01:42:56 -0400

    upgrade packet

--- app/src/androidTest/java/com/example/uscatterbrain/ProtocolInstrumentedTest.java
+++ app/src/androidTest/java/com/example/uscatterbrain/ProtocolInstrumentedTest.java
@@ -18,6 +18,7 @@ import com.example.uscatterbrain.network.BlockSequencePacket;
 import com.example.uscatterbrain.network.LibsodiumInterface;
 import com.example.uscatterbrain.network.ScatterDataPacket;
 import com.example.uscatterbrain.network.ScatterSerializable;
+import com.example.uscatterbrain.network.UpgradePacket;
 import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -122,6 +123,13 @@ public class ProtocolInstrumentedTest {
                 .setProvides(p)
                 .build();
         return advertisePacket;
+    }
+
+    public UpgradePacket getUpgrade() {
+        return new UpgradePacket.Builder()
+                .setProvides(ScatterProto.Advertise.Provides.ULTRASOUND)
+                .setSessionID(1)
+                .build();
     }
 
     public BlockSequencePacket getSequencePacket(int seq) {
@@ -281,6 +289,21 @@ public class ProtocolInstrumentedTest {
             ByteArrayInputStream is = new ByteArrayInputStream(data);
             AdvertisePacket n = new AdvertisePacket(is);
             assertThat(n.getProvides().get(0), is(ScatterProto.Advertise.Provides.BLE));
+        } catch (IOException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void UpgradePacketWorks() throws TimeoutException {
+        UpgradePacket up = getUpgrade();
+
+        byte[] data = up.getBytes();
+
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream(data);
+            UpgradePacket nu = new UpgradePacket(is);
+            assertThat(nu.getProvies(), is(ScatterProto.Advertise.Provides.ULTRASOUND));
         } catch (IOException e) {
             Assert.fail();
         }
---
+++ app/src/main/java/com/example/uscatterbrain/network/UpgradePacket.java
@@ -0,0 +1,97 @@
+package com.example.uscatterbrain.network;
+
+import com.example.uscatterbrain.ScatterProto;
+import com.google.protobuf.ByteString;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class UpgradePacket implements ScatterSerializable {
+
+    private ScatterProto.Upgrade mUpgrade;
+    private int mSessionID;
+    private ScatterProto.Advertise.Provides mProvides;
+
+    private UpgradePacket(Builder builder) {
+        this.mProvides = builder.getProvies();
+        this.mSessionID = builder.getSessionID();
+        this.mUpgrade = ScatterProto.Upgrade.newBuilder()
+                .setProvides(mProvides)
+                .setSessionid(mSessionID)
+                .build();
+    }
+
+    public UpgradePacket(InputStream is) throws IOException {
+        this.mUpgrade = ScatterProto.Upgrade.parseDelimitedFrom(is);
+        this.mSessionID = this.mUpgrade.getSessionid();
+        this.mProvides = this.mUpgrade.getProvides();
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return this.mUpgrade.toByteArray();
+    }
+
+    @Override
+    public ByteString getByteString() {
+        return this.mUpgrade.toByteString();
+    }
+
+    @Override
+    public boolean writeToStream(OutputStream os) {
+        try {
+            this.mUpgrade.writeTo(os);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return this.mUpgrade.toByteString().size();
+    }
+
+    public int getSessionID() {
+        return this.mSessionID;
+    }
+
+    public ScatterProto.Advertise.Provides getProvies() {
+        return this.mProvides;
+    }
+
+    public static class Builder {
+        private int mSessionid;
+        private ScatterProto.Advertise.Provides mProvides;
+
+        public Builder() {
+
+        }
+
+        public Builder setSessionID(int sessionID) {
+            this.mSessionid = sessionID;
+            return this;
+        }
+
+        public Builder setProvides(ScatterProto.Advertise.Provides provides) {
+            this.mProvides = provides;
+            return this;
+        }
+
+        public int getSessionID() {
+            return this.mSessionid;
+        }
+
+        public ScatterProto.Advertise.Provides getProvies() {
+            return this.mProvides;
+        }
+
+        public UpgradePacket build() {
+            if (mProvides == null || mSessionid <= 0)
+                return null;
+
+            return new UpgradePacket(this);
+        }
+    }
+}
