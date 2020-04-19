8ed6ab9c7d2e6698b03e2e57d1954221dd8c81f2
Alex Ballmer <gnu3ra@riseup.net>
2020-04-19 17:43:31 -0400

    switch to ScheduledExecutorService on thread pool

--- app/src/main/java/com/example/uscatterbrain/scheduler/ScatterbrainScheduler.java
+++ app/src/main/java/com/example/uscatterbrain/scheduler/ScatterbrainScheduler.java
@@ -1,9 +1,5 @@
 package com.example.uscatterbrain.scheduler;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
-
 import com.example.uscatterbrain.eventbus.events.BlockDataTransactionEvent;
 import com.example.uscatterbrain.network.BlockHeaderPacket;
 
@@ -12,10 +8,84 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-public class ScatterbrainScheduler extends Thread {
+public class ScatterbrainScheduler {
     static final String TAG = "Scheduler";
     private static ScatterbrainScheduler mInstance;
+    private ScheduledExecutorService mExecutor;
+    private long mUpdateDelay;
+    private RoutingServiceState mState;
+    private ScheduledFuture mFuture;
+
+    private ScatterbrainScheduler() {
+        this.mState = RoutingServiceState.STATE_SUSPEND;
+        this.mExecutor = Executors.newScheduledThreadPool(2);
+        this.mUpdateDelay = 10;
+    }
+
+    public static ScatterbrainScheduler getInstance() {
+        if (mInstance == null) {
+            mInstance = new ScatterbrainScheduler();
+        }
+        return mInstance;
+    }
+    public RoutingServiceState getRoutingServiceState() {
+        return this.mState;
+    }
+
+    private RoutingServiceState scheduleOnce() {
+        return null;
+    }
+
+    public void start() {
+        mFuture = mExecutor.scheduleAtFixedRate(() -> {
+
+        }, 0, mUpdateDelay, TimeUnit.SECONDS);
+    }
+
+    public boolean stop() {
+        if (mFuture != null) {
+            return mFuture.cancel(false);
+        }
+        return true;
+    }
+
+    public void sendBlockData(List<BlockHeaderPacket> packets) {
+        BlockDataTransactionEvent event = new BlockDataTransactionEvent.BlockDataTransactionEventBuilder("")
+                .setContents(packets)
+                .setTransactionType(BlockDataTransactionEvent.TransactionType.BD_TRANSACTION_SEND)
+                .build();
+
+        EventBus.getDefault().post(event);
+    }
+
+    @Subscribe(threadMode = ThreadMode.BACKGROUND)
+    public void onReceiveBlockdata(BlockDataTransactionEvent event) {
+        if(event.getmTransactionType() == BlockDataTransactionEvent.TransactionType.BD_TRANSACTION_RECEIVE) {
+            this.mExecutor.execute(() -> {
+                //ScatterMessage message = new ScatterMessage();
+                //TODO: convert from blockdata/blocksequence to scattermessage
+            });
+        }
+    }
+
+    public static class InvalidStateChangeException extends Exception {
+        private String mState;
+
+        public InvalidStateChangeException(String state) {
+            mState = state;
+        }
+
+        @Override
+        public String toString() {
+            return mState;
+        }
+    }
+
     private enum RoutingServiceState {
         STATE_DISCOVER_PEERS {
             @Override
@@ -96,98 +166,5 @@ public class ScatterbrainScheduler extends Thread {
         }
     }
 
-    private RoutingServiceState mState;
-    private Handler mHandler;
-    private int mTickDelay;
-
-    private ScatterbrainScheduler() {
-        this.mState = RoutingServiceState.STATE_SUSPEND;
-        this.mTickDelay = 1000;
-    }
-
-    public static ScatterbrainScheduler getInstance() {
-        if (mInstance == null) {
-            mInstance = new ScatterbrainScheduler();
-        }
-        return mInstance;
-    }
-
-    public void setTickDelay(int delay) {
-        this.mTickDelay = delay;
-    }
-
-    public int getTickDelay() {
-        return this.mTickDelay;
-    }
-
-    public RoutingServiceState getRoutingServiceState() {
-        return this.mState;
-    }
-
-    private RoutingServiceState scheduleOnce() {
-        return null;
-    }
-
-    @Override
-    public void interrupt() {
-        EventBus.getDefault().unregister(this);
-        super.interrupt();
-    }
-
-    @Override
-    public void run() {
-        Looper.prepare();
-
-        EventBus.getDefault().register(this);
-
-        mHandler = new Handler();
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                RoutingServiceState tmpState = scheduleOnce();
-                if (tmpState == null) {
-                    Log.e(TAG, "Attempted an invalid state change. Aborting");
-                    return;
-                }
-                mState = tmpState;
-                mHandler.postDelayed(this, mTickDelay);
-            }
-        });
-
-        Looper.loop();
-        EventBus.getDefault().unregister(this);
-    }
-
-    public void sendBlockData(List<BlockHeaderPacket> packets) {
-        BlockDataTransactionEvent event = new BlockDataTransactionEvent.BlockDataTransactionEventBuilder("")
-                .setContents(packets)
-                .setTransactionType(BlockDataTransactionEvent.TransactionType.BD_TRANSACTION_SEND)
-                .build();
-
-        EventBus.getDefault().post(event);
-    }
-
-    @Subscribe(threadMode = ThreadMode.BACKGROUND)
-    public void onReceiveBlockdata(BlockDataTransactionEvent event) {
-        if(event.getmTransactionType() == BlockDataTransactionEvent.TransactionType.BD_TRANSACTION_RECEIVE) {
-            this.mHandler.post(() -> {
-                //ScatterMessage message = new ScatterMessage();
-                //TODO: convert from blockdata/blocksequence to scattermessage
-            });
-        }
-    }
-
-    public static class InvalidStateChangeException extends Exception {
-        private String mState;
-
-        public InvalidStateChangeException(String state) {
-            mState = state;
-        }
-
-        @Override
-        public String toString() {
-            return mState;
-        }
-    }
 
 }
