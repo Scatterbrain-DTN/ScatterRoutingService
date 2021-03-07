c162c9602fc9993aaaf21b2efc6828f9dfe0f589
Alex Ballmer <gnu3ra@riseup.net>
2021-03-07 01:28:28 -0500

    bump room schema version

--- app/src/main/java/net/ballmerlabs/uscatterbrain/db/Datastore.kt
+++ app/src/main/java/net/ballmerlabs/uscatterbrain/db/Datastore.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import net.ballmerlabs.uscatterbrain.db.entities.*
 
-@Database(entities = [HashlessScatterMessage::class, KeylessIdentity::class, Hashes::class, MessageHashCrossRef::class, Keys::class, ClientApp::class], version = 2, exportSchema = false)
+@Database(entities = [HashlessScatterMessage::class, KeylessIdentity::class, Hashes::class, MessageHashCrossRef::class, Keys::class, ClientApp::class], version = 3, exportSchema = false)
 abstract class Datastore : RoomDatabase() {
     abstract fun identityDao(): IdentityDao
     abstract fun scatterMessageDao(): ScatterMessageDao
