3cb3b5d540a29dfc7bbb047ab65e9832b15df301
Alex Ballmer <gnu3ra@riseup.net>
2021-06-05 19:20:56 -0400

    fix BufferedOutputStream breaking outgoing packets

--- app/src/main/java/net/ballmerlabs/uscatterbrain/network/CRCOutputStream.kt
+++ app/src/main/java/net/ballmerlabs/uscatterbrain/network/CRCOutputStream.kt
@@ -1,23 +1,30 @@
 package net.ballmerlabs.uscatterbrain.network
 
-import java.io.BufferedOutputStream
 import java.io.OutputStream
 import java.util.zip.CRC32
 
-class CRCOutputStream(outputStream: OutputStream) : BufferedOutputStream(outputStream) {
+class CRCOutputStream(private val outputStream: OutputStream) : OutputStream() {
     val crc = CRC32()
     override fun write(p0: Int) {
         crc.update(p0)
-        super.write(p0)
+        outputStream.write(p0)
     }
 
     override fun write(b: ByteArray?) {
         crc.update(b)
-        super.write(b)
+        outputStream.write(b)
     }
 
     override fun write(b: ByteArray?, off: Int, len: Int) {
         crc.update(b, off, len)
-        super.write(b, off, len)
+        outputStream.write(b, off, len)
+    }
+
+    override fun close() {
+        outputStream.close()
+    }
+
+    override fun flush() {
+        outputStream.flush()
     }
 }
