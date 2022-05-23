f8e1f1cb96e747d5fdb3bbf7e014bfa1bc261f6c
Alex Ballmer <gnu3ra@riseup.net>
2022-05-22 21:00:40 -0500

    fix not setting timeout scheduler

--- app/src/main/java/net/ballmerlabs/uscatterbrain/network/bluetoothLE/server/GattServerImpl.kt
+++ app/src/main/java/net/ballmerlabs/uscatterbrain/network/bluetoothLE/server/GattServerImpl.kt
@@ -1,18 +1,25 @@
 package net.ballmerlabs.uscatterbrain.network.bluetoothLE.server
 
+import com.polidea.rxandroidble2.internal.operations.TimeoutConfiguration
+import io.reactivex.Scheduler
 import io.reactivex.Single
 import net.ballmerlabs.uscatterbrain.GattServerConnectionSubcomponent
+import net.ballmerlabs.uscatterbrain.RoutingServiceComponent
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import javax.inject.Named
 import javax.inject.Provider
 import javax.inject.Singleton
 
 @Singleton
 class GattServerImpl @Inject constructor(
-        private val connectionBuilder: Provider<GattServerConnectionSubcomponent.Builder>
+        private val connectionBuilder: Provider<GattServerConnectionSubcomponent.Builder>,
+        @Named(RoutingServiceComponent.NamedSchedulers.OPERATIONS) private val timeoutScheduler: Scheduler
 ): GattServer {
     override fun openServer(config: ServerConfig): Single<GattServerConnection> {
         return Single.fromCallable {
             connectionBuilder.get()
+                    .timeoutConfiguration(TimeoutConfiguration(10, TimeUnit.SECONDS, timeoutScheduler))
                     .serverConfig(config)
                     .build()!!.connection()
         }
