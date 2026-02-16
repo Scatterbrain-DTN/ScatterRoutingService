5ab6f1f47c79de29e9c56a1e3ab876fbe92fd7ad
fmeef <gnu3ra@riseup.net>
2026-02-16 02:53:19 -0600

    lock on queue

--- app/src/main/java/net/ballmerlabs/uscatterbrain/util/QueueSubject.kt
+++ app/src/main/java/net/ballmerlabs/uscatterbrain/util/QueueSubject.kt
@@ -10,6 +10,9 @@ import org.reactivestreams.Subscription
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
 class QueueItem<T>(
     val item: T?
 )
@@ -17,59 +20,72 @@ class QueueItem<T>(
 class QueueSubject<T>(): FlowableSubscriber<T> {
     val queue = LinkedBlockingQueue<QueueItem<T>>()
     val complete = AtomicBoolean(false)
+    val lock = ReentrantLock()
     val subscription = mutableSetOf<Subscription>()
     override fun onSubscribe(s: Subscription) {
-        subscription.add(s)
-        s.request(1)
+        lock.withLock {
+            subscription.add(s)
+            s.request(1)
+        }
     }
 
     override fun onNext(t: T?) {
-        queue.put(QueueItem(t))
-        subscription.forEach { s -> s.request(1) }
+        lock.withLock {
+            queue.put(QueueItem(t))
+            subscription.forEach { s -> s.request(1) }
+        }
     }
 
     override fun onError(t: Throwable?) {
-        complete.set(true)
+        lock.withLock {
+            complete.set(true)
+        }
     }
 
     fun hasItem(): Boolean {
         Log.v("debug","queue hasItem empty=${queue.isEmpty()} item=${queue.peek()?.item}")
-        return !(queue.isEmpty() || queue.peek()?.item == null)
+        lock.withLock {
+            return !(queue.isEmpty() || queue.peek()?.item == null)
+        }
     }
 
     fun getOrNull(): Single<QueueItem<T>> {
         return Single.defer {
-            if (complete.get() && (queue.isEmpty() || queue.peek()?.item == null)) {
-                Single.just(QueueItem(null))
-            } else {
-                val item = queue.poll()?.item
-                if (item != null)
-                    Single.just(QueueItem(item))
-                else
+            lock.withLock {
+                if (complete.get() && (queue.isEmpty() || queue.peek()?.item == null)) {
                     Single.just(QueueItem(null))
+                } else {
+                    val item = queue.poll()?.item
+                    if (item != null)
+                        Single.just(QueueItem(item))
+                    else
+                        Single.just(QueueItem(null))
+                }
             }
-
         }
     }
 
     fun get(): Maybe<T> {
         return Maybe.defer {
-            if (complete.get() && (queue.isEmpty() || queue.peek()?.item == null)) {
-                Maybe.empty()
-            } else {
-                val item = queue.poll()?.item
-                if (item != null)
-                    Maybe.just(item)
-                else
+            lock.withLock {
+                if (complete.get() && (queue.isEmpty() || queue.peek()?.item == null)) {
                     Maybe.empty()
+                } else {
+                    val item = queue.poll()?.item
+                    if (item != null)
+                        Maybe.just(item)
+                    else
+                        Maybe.empty()
+                }
             }
-
         }
     }
 
     override fun onComplete() {
         Log.e("debug", "queue complete?")
-       complete.set(true)
-        queue.put(QueueItem(null))
+        lock.withLock {
+            complete.set(true)
+            queue.put(QueueItem(null))
+        }
     }
 }
